bb173ac77456e4f5d6845d93902d6d2870633665
soumik <soumik@ITSL-DT15.insync.local>
2018-05-25 20:07:40 +0530

    test util java

--- src/main/java/com/magento/qa/util/TestUtil.java
+++ src/main/java/com/magento/qa/util/TestUtil.java
@@ -1,13 +1,15 @@
 package com.magento.qa.util;
 
-public class TestUtil {
+import com.magento.qa.base.TestBase;
+
+public class TestUtil extends TestBase{
 	
 	public static long Page_Load_Timeout= 20;
 	public static long Implicit_Wait= 10;
 	
-	//public void SwitchToframe() {
+	/*public void SwitchToframe() {
 		
-		
-	//}
+		driver.switchTo();
+	}*/
 
 }
