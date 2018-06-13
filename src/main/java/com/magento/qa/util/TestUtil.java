a8c82829fe2751eb967a36950817b5dcd23d81b4
soumik <soumik@ITSL-DT15.insync.local>
2018-06-13 13:53:00 +0530

    comment

--- src/main/java/com/magento/qa/util/TestUtil.java
+++ src/main/java/com/magento/qa/util/TestUtil.java
@@ -7,6 +7,8 @@ public class TestUtil extends TestBase{
 	public static long Page_Load_Timeout= 20;
 	public static long Implicit_Wait= 10;
 	
+	/*--this is using for difreent frame element to find 
+	
 	/*public void SwitchToframe() {
 		
 		driver.switchTo();
